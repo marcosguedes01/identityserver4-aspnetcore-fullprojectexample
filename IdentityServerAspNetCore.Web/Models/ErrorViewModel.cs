b14c5a66c66dbea438fdf8f704da1200badb366b
Marcos Guedes <mguedes@growin.pt>
2019-07-23 15:07:44 +0100

    Projeto Web adicionado.

---
+++ IdentityServerAspNetCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServerAspNetCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
