1a03b712442b66efc0226d02b8450b98b722c8e3
Marcos Guedes <mguedes@growin.pt>
2019-07-23 11:41:38 +0100

    projeto de api criado

--- IdentityServerAspNetCore.Api/Program.cs
+++ IdentityServerAspNetCore.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServerApnetCore.OAuth
+namespace IdentityServerAspNetCore.Api
 {
     public class Program
     {
@@ -21,7 +21,6 @@ namespace IdentityServerApnetCore.OAuth
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("https://localhost:52047");
                 });
     }
 }
